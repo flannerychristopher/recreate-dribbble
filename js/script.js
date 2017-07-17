93d706b38322927f52c528d64cec375f3b641590
Chris Flan <flanthedev@gmail.com>
2017-07-17 13:52:44 +1000

    mobile nav t-more toggles second menu

--- js/script.js
+++ js/script.js
@@ -4,4 +4,15 @@ $(document).ready(function(){
       $("#nav").toggleClass("nav-open");
   });
 
+  $("#t-more > a").click(function() {
+    $("#t-search, #t-shots, #t-players, #t-blog, #t-podcast, #t-meetups, #t-jobs, #t-more > a").hide();
+    $("#t-more-hide, #t-more .tabs").show();
+    $("#t-more > .tabs > li").addClass("separate");
+  })
+
+  $("#t-more-hide").click(function() {
+    $("#t-search, #t-shots, #t-players, #t-blog, #t-podcast, #t-meetups, #t-jobs, #t-more > a").show();
+    $("#t-more-hide, #t-more .tabs").hide();
+  })
+
 });
