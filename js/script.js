5cde37ce3771fb8a97654b89f2d7cef3e5613d22
Chris Flan <flanthedev@gmail.com>
2017-07-21 11:59:22 +0800

    progress on action bar

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ $(document).ready(function(){
       $("#nav").toggleClass("nav-open");
   });
 
-  $("#t-more > a").click(function() {
+  $("#t-more").click(function() {
     $("#t-search, #t-shots, #t-players, #t-blog, #t-podcast, #t-meetups, #t-jobs, #t-more > a").hide();
-    $("#t-more-hide, #t-more .tabs").show();
+    $("#t-more-back, #t-more .tabs").show();
     $("#t-more > .tabs > li").addClass("separate");
   })
 
-  $("#t-more-hide").click(function() {
-    $("#t-search, #t-shots, #t-players, #t-blog, #t-podcast, #t-meetups, #t-jobs, #t-more > a").show();
-    $("#t-more-hide, #t-more .tabs").hide();
+  $("#t-more-back").click(function() {
+    $("#t-search, #t-shots, #t-players, #t-blog, #t-podcast, #t-meetups, #t-jobs, #t-more> a").show();
+    $("#t-more-back, #t-more .tabs").hide();
   })
 
 });
